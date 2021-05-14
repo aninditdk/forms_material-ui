2f94a20b13a9cf9c927e1e7881afee6a5b0b4965
Anindit <aninditdk@gmail.com>
2021-05-14 18:23:36 +0530

    Removed unnecessary code in App.jss

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { QueryClient,QueryClientProvider } from 'react-query'
 import FormData from './FormData'
-import Tabledata from './Tabledata';
+
 
 const queryClient = new QueryClient();
 function App() {
